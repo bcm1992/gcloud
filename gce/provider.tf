002ca3f5d0e718c0be6dbd3c8e5bc6a0ea38f802
Jun Makino <bcm1992@Juns-MacBook-Pro-2016.local>
2020-04-27 14:26:19 -0700

    move tfstate to oci object storage

--- gce/provider.tf
+++ gce/provider.tf
@@ -3,3 +3,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-b"
 }
+terraform {
+  backend "http" { 
+    address = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/9G3K5DO8X4o5A-h_7pKSPfDS4QGeEoQm8rxDY40HGfo/n/axtee0lnkutf/b/terraform-gcloud/o/terraform-gce.tfstate"
+    update_method = "PUT" 
+  }
+}
